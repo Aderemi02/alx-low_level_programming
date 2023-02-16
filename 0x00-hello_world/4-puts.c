602fd61660bf793a25162703b889f513364e86d4
Adekunle <omoremilekun@gamil.com>
2023-02-16 09:26:09 -0800

    outputing with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-/* this is used to output strings using puts*/
+/**
+ * this is used to output strings using puts
+ */
 int main(void)
 	{
 	puts("\"Programming is like building a multilingual puzzle");
