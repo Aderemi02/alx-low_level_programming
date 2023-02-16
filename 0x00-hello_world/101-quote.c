9515f5d145c250f6433f225472bebb5f4ac73fbd
Adekunle <omoremilekun@gamil.com>
2023-02-16 14:54:20 -0800

    outputting with write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,11 @@
 #include <unistd.h>
 
 /**
- *main: this is used to print using putchar
- *write: this is a function used to print the output
- *Description: the write outputs using the begining and the end numbers
- *Return: 1 is returned after the function is executed
+ * main: this is used to print using write function
+ * write: this is a function used to print the output
+ *
+ * Description: the write outputs using the begining and the end numbers
+ * Return: 1 is returned after the function is executed
  */
 
 int main(void)
