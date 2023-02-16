ad59961eea6c7bf9c16cee6b11248acec8052537
Adekunle <omoremilekun@gamil.com>
2023-02-16 09:31:46 -0800

    outputing with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
- * this is used to output strings using puts
+ * function_main - This is used to help out put the strings
+ * puts - this is a function used to output a string
+ * return (0): this return a value of 0 to the function main
  */
 int main(void)
 	{
