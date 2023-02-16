f8dc302a40dd28907fb299f6c3d362767b123588
Adekunle <omoremilekun@gamil.com>
2023-02-16 12:03:41 -0800

    printing using putchar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main: this is used to print using putchar
+ *putchar: this is a function used to print the otuput
+ *Return: 1 is returned after the function is executed
+ */
+
+int main(void)
+{
+	putchar("and that piece of art is useful\" \- Dora Korpar\, 2015\-10\-19");
+	return (1);
+}
