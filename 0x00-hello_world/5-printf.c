5995eb102094910cc9a4baab5fbb738124630099
Adekunle <omoremilekun@gamil.com>
2023-02-16 09:43:09 -0800

    outputting using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This is used to output the strings using printf
+ * printf - this is a function used to output a string
+ * Return: 0 is to the function main
+ */
+int main(void)
+	{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+	}
