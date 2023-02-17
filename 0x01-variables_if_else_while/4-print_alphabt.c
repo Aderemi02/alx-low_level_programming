4e60f7cd78d11ab387a076b402b545cb20552f48
Adekunle <omoremilekun@gamil.com>
2023-02-17 14:52:22 -0800

    skipping some alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: return 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'e' || c == 'q')
+			continue;
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
