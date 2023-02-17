9d21849498ea8e575e82205881b42c5c84ed7d0d
Adekunle <omoremilekun@gamil.com>
2023-02-17 14:42:00 -0800

    possible comibination of a single digit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-
 /**
- *main - Block of character
- *Return: always 0 success
+ * main -main block
+ * Description: to print number combination
+ * Return: 0 when successful
  */
 
 int main(void)
 {
 	int c = 0;
-	{
 	for (c = 0; c <= 9; ++c)
+	{
 		putchar('0' + c);
 		if (c < 9)
 		{
