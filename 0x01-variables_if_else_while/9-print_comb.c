1cf991c4b0f04b00a3bf57fd7fb8e5ce4450d31b
Adekunle <omoremilekun@gamil.com>
2023-02-17 14:01:56 -0800

    possible comibination of a single digit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - Block of character
+ *Return: always 0 success
+ */
+
+int main(void)
+{
+        int c = 0;
+	for (c = 0; c <= 9; ++c)
+	{
+                        putchar('0' + c);
+
+			if (c < 9)
+			{
+				putchar(',');
+			putchar(' ');
+		}
+	}			
+			putchar('\n');
+			return (0);
+}
