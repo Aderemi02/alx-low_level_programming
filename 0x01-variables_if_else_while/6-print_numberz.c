77abef3607d1852f0b2b984ee76e2fbab7a5bb81
Adekunle <omoremilekun@gamil.com>
2023-02-17 12:40:32 -0800

    printing numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - Block of character
+ *Return: always 0 success
+ */
+
+int main(void)
+{
+        int c = 0;
+        {
+                for (int c = 0; c <= 9; ++c)
+			putchar('0' + c);
+                putchar('\n');
+        }
+return (0);
+}
+
