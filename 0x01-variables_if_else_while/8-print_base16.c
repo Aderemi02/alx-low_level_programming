5554d631a9a50263a16db546a5fdc0417f9e972a
Adekunle <omoremilekun@gamil.com>
2023-02-17 13:21:41 -0800

    printing bae16 numbers in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,15 +8,15 @@
 int main(void)
 {
 	int c = 0;
-        {
-                for (c = 0; c <= 9; ++c)
-                        putchar('0' + c);
+	{
+		for (c = 0; c <= 9; ++c)
+			putchar('0' + c);
 	}
 	{
 		for (c=49; c <= 54; ++c)
 			putchar('0' + c);
-                printf("\n");
-        }
+		printf("\n");
+	}
 return (0);
 }
 
