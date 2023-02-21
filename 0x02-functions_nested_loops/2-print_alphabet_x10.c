f927fadc2c48078917fa4a27751f40437fcdfecf
Adekunle <omoremilekun@gamil.com>
2023-02-21 09:32:33 -0800

    printing alphabets x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ */
+
+void print_alphabet_x10(void)
+	char c = 'a';
+	int i;
+	do {
+		for (c = 'a'; c <= 'z'; c++)
+			putchar(c);
+	}
+putchar('\n');
+while (i <= 10);
