3e53135e174b986728355de37123c822cd9f6b2d
Adekunle <omoremilekun@gamil.com>
2023-02-21 10:58:32 -0800

    checking lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <ctype.h>
+
+/**
+ *
+ *
+ */
+
+int _islower(int c)
+{
+	int c;
+	char d[] ="abcdefghijklmnopqrstuvwxyz"
+	c = islower(d[])
+	putchar('0' + c);
+	putchar('\n');
+	return 1;
+}
