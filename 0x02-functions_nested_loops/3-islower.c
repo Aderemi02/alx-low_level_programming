81039cc567bd937bafdacebb2074c50a8d96f2ca
Adekunle <omoremilekun@gamil.com>
2023-02-21 12:39:16 -0800

    returning lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,7 +5,9 @@
  * _islower - used to identify lowercase
  * int c - used for accepting the value
  * _islower(c) - accepts the value of c to check if its lowercase
+ *c - accepts and compare value 
  * return: 1 if lowercase.
+ * return: 0 if false
  */
 
 int _islower(int c)
