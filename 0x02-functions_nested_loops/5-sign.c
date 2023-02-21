4149a620107ddb2c7b20d90b4f7c861fbcfb321d
Adekunle <omoremilekun@gamil.com>
2023-02-21 13:50:33 -0800

    check for signed character

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_sign - find the signed value
+ *Return: 1 if positive, -1 if negative and 0 otherwise
+ */
+
+int print_sign(int n)
+	if (n > 0)
+	{
+	putchar('+');
+	return (1);
+	}
+	else if (n < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+	else
+	{
+		return(0);
+	}
