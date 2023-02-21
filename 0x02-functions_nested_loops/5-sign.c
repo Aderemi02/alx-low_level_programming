814ba96d89ac8b739b45fe45e36e7be30b71080a
Adekunle <omoremilekun@gamil.com>
2023-02-21 14:28:28 -0800

    check for signed character

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,17 +7,20 @@
 
 int print_sign(int n)
 {
+	int c = +1;
+	int b = -1;
+	int a = 0;
 	if (n > 0)
 	{
-	putchar('+');
+	putchar('c');
 	return (1);
 	}
 	else if (n < 0)
 	{
-	putchar('-');
+	putchar('b');
 	return (-1);
 	}
 	else
-	putchar('0');
+	putchar('a');
 	return(0);
 }
