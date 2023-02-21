6507da3a798916a28d0fbac77032be11cf8bea97
Adekunle <omoremilekun@gamil.com>
2023-02-21 13:04:24 -0800

    printing lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,9 +3,10 @@
 #include <unistd.h>
 /**
  * _islower - used to identify lowercase
+ * _islower(int c) - return 1 value
  * int c - used for accepting the value
  * _islower(c) - accepts the value of c to check if its lowercase
- * c: - accepts and compare value
+ * c - accepts and compare value
  * return: 0 if false
  */
 
