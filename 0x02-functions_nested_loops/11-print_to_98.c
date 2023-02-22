dfbb7cf48dee1ef210e302a074d225c6560c690d
Adekunle <omoremilekun@gamil.com>
2023-02-21 18:19:02 -0800

    print natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+
+void print_to_98(int n)
+{
+	{
+		for (n = n; n <= 98; n++)
+	{
+		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+	}
+	}
+	{
+		for (n = n; n >= 98; n--)
+		{
+		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+		}		
+	}
+}
+		
