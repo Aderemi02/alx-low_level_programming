9ea4d5e80da90f8f2256ac924b1a8931afbcde3f
Adekunle <omoremilekun@gamil.com>
2023-02-21 17:45:18 -0800

    add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+
+int add(int, int)
+{
+	int first_int;
+	int second_int;
+	int result;
+	result = first_int + second_int;
+	{
+		_putchar(result + '0');
+	}
+	return (result);
+}
