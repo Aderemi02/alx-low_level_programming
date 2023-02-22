f8cf468991d8e9a3595eaa94e3ad73f338e57796
Adekunle <omoremilekun@gamil.com>
2023-02-21 17:54:16 -0800

    add two integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
- *
- *
+ * add - Adds two values
+ * @first_int: The first number to be computed
+ * @second_int: The second number to be computed
+ * Return: Integer value
  */
 
 int add(int first_int, int second_int)
 {
 	int result;
 	result = first_int + second_int;
-	{
-	putchar(result + '0');
-	}
 	return (result);
 }
