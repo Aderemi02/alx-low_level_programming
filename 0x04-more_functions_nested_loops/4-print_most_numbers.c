407493226533c34ceb7257487a3b1f516b2bef28
Adekunle <omoremilekun@gamil.com>
2023-02-23 04:56:01 -0800

    omitting 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - omits 2 and 4
+ * @i - integer for iteration
+ * Return: Ano value returned
+ */
+void print_most_numbers(void)
+{
+	int i;
+	
+	for (i = 0; i <= 9; i++)
+	{
+		if ((i != 2) && (i != 4))
+		{
+			_putchar(i + '0');
+		}
+	}
+	putchar('\n');
+}
