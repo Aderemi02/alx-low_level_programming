284d763838f665ffa4ba85d4fa52e25b1b21de78
Adekunle <omoremilekun@gamil.com>
2023-02-23 15:40:17 -0800

    ommitting 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,7 +7,7 @@
 void print_most_numbers(void)
 {
 	int i;
-	
+
 	for (i = 0; i <= 9; i++)
 	{
 		if ((i != 2) && (i != 4))
@@ -15,5 +15,5 @@ void print_most_numbers(void)
 			_putchar(i + '0');
 		}
 	}
-	putchar('\n');
+	_putchar('\n');
 }
