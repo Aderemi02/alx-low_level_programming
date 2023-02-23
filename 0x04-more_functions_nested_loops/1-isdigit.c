d0cf809aa7fb7a9b1036e1b26b28928686c4be69
Adekunle <omoremilekun@gamil.com>
2023-02-23 02:44:11 -0800

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - writes the parameter c to stdout
+ * @c: is the int to be checked
+ * Return: 1 if int is a digit, otherwise 0.
+ */
+
+int _isdigit(int c)
+{
+	if ((c = 0) && (c <= 9))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
