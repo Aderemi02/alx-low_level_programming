3577df07759271e7a2b4192577c24840290a8245
Adekunle <omoremilekun@gamil.com>
2023-02-23 15:51:10 -0800

    drawing lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,14 +6,14 @@
  */
 void print_line(int n)
 {
-        while ( n > 0)
-        {
-        if (n <= 0)
-                {
-                        _putchar('\n');
-                }
-                _putchar(95);
-                n--;
-        }
+	while (n > 0)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		_putchar(95);
+		n--;
+	}
 	_putchar('\n');
 }
