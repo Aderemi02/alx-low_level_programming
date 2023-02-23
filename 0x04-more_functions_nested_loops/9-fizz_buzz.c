66f51260e219b1b80a52c052cd7cff2d4ed7583e
Adekunle <omoremilekun@gamil.com>
2023-02-23 09:44:30 -0800

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - writes the FizzBuzz
+ * @a - used to iterate integer from 1 - 100
+ * Return: 0 when true.
+ */
+int main (int a)
+{
+	for (a = 1; a <= 100; a++)
+	{
+		if ((a % 15) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			if ((a % 3) == 0)
+			{
+				printf("Fizz ");
+			}
+			else
+			{
+				if ((a % 5) == 0)
+				{
+					printf("Buzz ");
+				}
+				else
+				{
+					printf("%d ", a);
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
