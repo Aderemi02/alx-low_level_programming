f3fba3fd04d6476bbac4effd6936f2a57ec2c76a
Adekunle <omoremilekun@gamil.com>
2023-02-23 16:07:36 -0800

    diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,35 +1,33 @@
 #include "main.h"
 /**
  * print_diagonal - print diagonal line
- * @n - used to parse the number of diagonal line
- * @i - to iterate printing of line
- * @j - to iterate spaces before diagonal
+ * @n: - used to parse the number of diagonal line
  * Return: no value returned.
  */
 void print_diagonal(int n)
 {
-        if (n <= 0)
-                {
-                        _putchar('\n');
-                }
-        else
-        {
-                int i,j;
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
 
-                for (i = 0; i < n; i++)
-                {
-                        for (j = 0; j < n; j++)
-                        {
-                                if (j == i)
-                                {
-                                        _putchar(92);
-                                }
-                                else if ( j < i)
-                                {
-                                        _putchar(' ');
-                                }
-                        }
-                        _putchar('\n');
-                }
-        }
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+				{
+					_putchar(92);
+				}
+				else if (j < i)
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
