40bbc96c7f02df9e32e34b3606bfafbf6f1bf375
Adekunle <omoremilekun@gamil.com>
2023-02-23 18:15:08 -0800

    prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -12,13 +12,13 @@ int main(void)
 	long a, i;
 
 	a = 612852475143;
-	for (i = 2; i < n; i++)
+	for (i = 2; i < a; i++)
 	{
 		while (a % i == 0)
 			a = a / i;
 	}
 
-	printf("%lu\n", n);
+	printf("%lu\n", a);
 
 	return (0);
 }
