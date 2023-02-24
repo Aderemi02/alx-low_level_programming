fb0d92b944c56c5cd48a6ab1042f47b5e6083583
Adekunle <omoremilekun@gamil.com>
2023-02-23 16:16:22 -0800

    squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,28 +1,26 @@
 #include "main.h"
 /**
  * print_square - prints square usinc #
- * @size - used to define the size of square
- * @i - used to iterate the length of the square
- * @j - used to iterate the breath of the square
+ * @size: - used to define the size of square
  */
 void print_square(int size)
 {
-        if (size <= 0)
-        {
-                _putchar('\n');
-        }
-        else
-        {
-                int i,j;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
 
-                for (i = 0; i < size; ++i)
-                {
-                        for (j = 1; j < size; ++j)
-                        {
-                                _putchar(35);
-                        }
-                        _putchar(35);
-                        _putchar('\n');
-                }
-        }
+		for (i = 0; i < size; ++i)
+		{
+			for (j = 1; j < size; ++j)
+			{
+				_putchar(35);
+			}
+			_putchar(35);
+			_putchar('\n');
+		}
+	}
 }
