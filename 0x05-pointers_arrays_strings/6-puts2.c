7ca40a18e8868949c21370ea336523a039676493
Adekunle <omoremilekun@gamil.com>
2023-02-27 15:22:03 -0800

    printing special string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,10 @@ void puts2(char *str)
 {
 	int c;
 
-	for (c = 0; str[c] != '\0'; a++)
+	for (c = 0; str[c] != '\0'; c++)
 	{
 		if (c % 2 == 0)
-			_putchar (str[a]);
+			_putchar (str[c]);
 	}
 
 	_putchar ('\n');
