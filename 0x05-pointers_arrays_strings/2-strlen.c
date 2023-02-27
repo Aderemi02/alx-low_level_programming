865d209dc0264ff9f85f99986c570d91cfe52269
Adekunle <omoremilekun@gamil.com>
2023-02-27 08:15:21 -0800

    strig length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - finds length of string
+ * a: stores length of string
+ * @s: pass a string
+ * Return: returns string length.
+ */
+
+int _strlen(char *s)
+{
+	int a;
+
+	a = strlen(s);
+	return (a);
+}
