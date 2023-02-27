c838ad2ae1b42b592abac65935b4de156c2a048a
Adekunle <omoremilekun@gamil.com>
2023-02-27 07:29:00 -0800

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets and pointer passed to 98
+ *
+ * @n: the recieving parameter
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+welcome to the readme file for pointers, arrays and strings.
+
+here you will be learning more about POINTERS, ARRAYS AND STRINGS WITH CODES
