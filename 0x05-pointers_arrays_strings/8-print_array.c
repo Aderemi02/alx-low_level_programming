704ee4757a75bd7062cd1f801b11198b1c759516
Adekunle <omoremilekun@gamil.com>
2023-02-27 14:21:50 -0800

    printing arrays

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,16 +4,16 @@
  * print_array - prints array elements
  * @a: pass array name
  * @n: pass array size
- * @t: used for iteration
  */
 
 void print_array(int *a, int n)
 {
 	int t;
+
 	for (t = 0; t < n; t++)
 	{
 		printf("%d", a[t]);
-	if (t != (n-1))
+	if (t != (n - 1))
 	{
 		printf(", ");
 	}
