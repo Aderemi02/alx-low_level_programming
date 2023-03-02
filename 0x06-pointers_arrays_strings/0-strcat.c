3b5bbd6dc6d178ca5fced78fd9e831fe7f41c8e2
Adekunle <omoremilekun@gamil.com>
2023-03-02 02:41:05 -0800

    concatenating two strings with n source byte

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,23 +9,16 @@ char *_strcat(char *dest, char *src)
 {
 	int i = 0;
 	int destlen = 0;
-	int srclen = 0;
-	char *p;
-
-	while ( src[srclen] != '\0')
-	{
-		srclen++;
-	}
 
 	while (dest[destlen] != '\0')
 	{
 		destlen++;
 	}
 
-	for ( i = (destlen -1); dest[destlen] != '\0'; i++)
+	for ( i = 0; src[i] != '\0'; i++)
 	{
-		dest[i] = src[srclen];
+		dest[destlen + i] = src[i];
 	}
-	p = &(dest[i]);
-	return(p);
+	dest[destlen + i] = '\0';
+	return(dest);
 }
