c8fa53cbceca3377e58e564a8cae60e8bf4e33e8
Adekunle <omoremilekun@gamil.com>
2023-03-02 03:32:55 -0800

    comparing two stings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcmp - compare two strings
+ * @s1: the first string
+ * @s2: the second string
+ * Return: returns the value of i
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	if (s1[0] < s2[0])
+	{
+		i = s1[0] - s2[0];
+	}
+	else
+		if (s1[0] > s2[0])
+		{
+			i = s1[0] - s2[0];
+		}
+		else
+		{
+			i = 0;
+		}
+	return (i);
+}
