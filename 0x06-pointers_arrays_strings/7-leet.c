334ccbaa1338e1bb28e7513db679c8defb1078f1
Adekunle <omoremilekun@gamil.com>
2023-03-02 17:16:04 -0800

    switching strings for numbers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,26 +1,26 @@
 #include "main.h"
 /**
  * *leet - used to switch characters for numbers
- * @str: used to point to string in the main function
+ * @s: used to point to string in the main function
  *
- * Return: str is returned.
+ * Return: s is returned.
  */
 
-char *leet(char *str)
+char *leet(char *s)
 {
 	int i, j;
 	char p[] = "aAeEoOtTlL";
 	char q[] = "4433007711";
 	
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (str[i] == p[j])
+			if (s[i] == p[j])
 			{
-				str[i] = q[j];
+				s[i] = q[j];
 			}
 		}
 	}
-	return (str);
+	return (s);
 }
