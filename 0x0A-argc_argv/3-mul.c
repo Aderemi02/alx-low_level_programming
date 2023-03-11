90b6e271f6a431627164ee99daed9a91a16e3b9c
Adekunle <omoremilekun@gamil.com>
2023-03-10 17:15:40 -0800

    multiplying

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,22 +4,22 @@
  * main - multiply argument
  * @argc: count of argument
  * @argv: pointer array to each count
- * Return: 1 if success
+ * Return: 0 if success
  */
 
 int main(int argc, char *argv[])
 {
+	int mul;
+
 	if (argc <= 2)
 	{
 		printf("Error\n");
+		return (1);
 	}
-
-	if (argc == 3)
+	else
 	{
-		int mul;
 		mul = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n",mul);
+		printf("%d\n", mul);
 	}
-
-	return (1);
+	return (0);
 }
