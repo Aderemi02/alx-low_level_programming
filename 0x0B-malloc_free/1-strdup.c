49081455bdcf6e9dfc5290ed1227ec8177453918
Adekunle <omoremilekun@gamil.com>
2023-03-14 08:05:07 -0700

    copying string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - duplicate a string
+ * @str: string to be duplicated
+ * Return: pointer if success
+ */
+
+char *_strdup(char *str)
+{
+	char *p;
+	int i, j;
+
+	if (str == NULL)
+	{
+		return (0);
+	}
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	p = malloc(sizeof(*p) * i);
+
+	j = 0;
+
+	while (str[j] != '\0')
+	{
+		p[j] = str[j];
+		j++;
+	}
+	return (p);
+	free(p);
+
+	return (0);
+}
+
+
