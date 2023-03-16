209c1be860336e406872b500eaf92eae1a9e2fa8
Adekunle <omoremilekun@gamil.com>
2023-03-16 09:17:28 -0700

    string concatenation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		m++;
 	}
 
-	p = malloc(sizeof(*p) * (n + i) - 1);
+	p = malloc(sizeof(*p) * (n + i) );
 	if (p == NULL)
 	{
 		return (0);
@@ -41,7 +41,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		p[l] = s1[l];
 	}
-	for (; a < n; a++, l++)
+	for (; a < (n + 1); a++, l++)
 	{
 		p[l] = s2[a];
 	}
