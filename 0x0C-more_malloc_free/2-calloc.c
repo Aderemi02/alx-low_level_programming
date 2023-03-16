21a79dc97dc08e3b9e60e6cb79a6d1f48855b105
Adekunle <omoremilekun@gamil.com>
2023-03-16 10:07:54 -0700

    creating array and initializing using malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - create an array and initialize to 0
+ * @nmemb: number of elements to be created
+ * @size: size of each element in byte
+ * Return: arry when success
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *arry;
+	unsigned int a;
+
+	if (nmemb == 0)
+		return (NULL);
+
+	if (size == 0)
+		return (NULL);
+
+	arry = malloc(nmemb * size);
+
+	if (arry == NULL)
+		return (NULL);
+
+	for (a = 0; a <= (nmemb * size); a++)
+		arry[a] = 0;
+
+	return (arry);
+}
