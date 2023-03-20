636cbc07675e66fe000dc3f1f55a8a776b1a2bff
Adekunle <omoremilekun@gamil.com>
2023-03-20 14:37:48 -0700

    structure for dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+introduction to structure and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for dog
+ * @name: a pointer to name
+ * @age: stores age
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
