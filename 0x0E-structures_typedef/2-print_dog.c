a062f0e7f5c9d760a6abe9d464502b9ac7d1c38e
Adekunle <omoremilekun@gamil.com>
2023-03-20 15:52:27 -0700

    printing dogs in format

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - printing a format
+ * @d: pointer to the struct my_dog
+ */
+void print_dog(struct dog *d)
+{
+	if ((d) == NULL)
+	{
+		printf("\n");
+	}
+	if ((*d).name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", (*d).name);
+
+	if ((*d).age == '\0')
+		printf("Age: %f\n", (*d).age);
+	else
+		printf("Age: %f\n", (*d).age);
+
+	if ((*d).owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", (*d).owner);
+}
