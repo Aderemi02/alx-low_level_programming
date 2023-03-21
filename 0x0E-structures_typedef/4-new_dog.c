be3a1632f5f3323cc91a329c122c70464f1c7008
Adekunle <omoremilekun@gamil.com>
2023-03-20 17:05:41 -0700

    creating new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,51 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * new_dog - creating a new dog.
+ * @name: name of the struct.
+ * @age: age of the struct.
+ * @owner: owner of the struct.
+ * Return: struct dog.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p;
+	int a, pname, powner;
+
+	p = malloc(sizeof(*p));
+
+	if ((p == NULL) || (name == NULL) || (age == '\0') || (owner == NULL))
+	{
+		free(p);
+		return (NULL);
+	}
+
+	for (pname = 0; name[pname]; pname++)
+	{
+		for (powner = 0; owner[powner]; powner++)
+		{
+			(*p).name = malloc(pname + 1);
+			(*p).owner = malloc(powner + 1);
+		}
+	}
+
+	if (((*p).name == NULL) || ((*p).owner == NULL))
+	{
+		free((*p).owner);
+		free((*p).name);
+		free(p);
+		return (NULL);
+	}
+
+	for (a = 0; a < pname; a++)
+		(*p).name[a] = name[a];
+	(*p).name[a] = '\0';
+
+	(*p).age = age;
+
+	for (a = 0; a < powner; a++)
+		(*p).owner[a] = owner[a];
+	(*p).owner[a] = '\0';
+
+	return (p);
+}
