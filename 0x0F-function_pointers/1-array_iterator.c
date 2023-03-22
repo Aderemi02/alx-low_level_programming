b8c847aea1aface7fc7de3c4302627edc67fe938
Adekunle <omoremilekun@gamil.com>
2023-03-22 15:38:25 -0700

    array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - function to call function pointer
+ * @array: array of the function pointer
+ * @size: size of the array
+ * @action: function pointer
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (!size)
+	{
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+	action(array[i]);
+	}
+}
