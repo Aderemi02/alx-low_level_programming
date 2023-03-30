4ac3ed58103afacc0b5f3a3c975e379ed99997ff
Aderemi <omoremilekun@gmail.com>
2023-03-30 09:16:05 -0700

    printing list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_list - function to return number of nodes
+ * @h: pointer to the list of valuesi
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int n = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef LIST_H
+#define LIST_H
+#include <stddef.h>
+#include <stdio.h>
+
+typedef struct singly list_t;
+
+/**
+ * struct singly - defining the datatype
+ * @str: pointer to a string
+ * @len: length of the string
+ * @next: pointer to the next node
+ */
+
+struct singly
+{
+	char *str;
+	int len;
+	struct singly *next;
+};
+
+size_t print_list(const list_t *h);
+
+
+#endif
