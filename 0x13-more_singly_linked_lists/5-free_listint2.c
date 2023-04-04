3ce07d38c6554b5f3167579be88718d16b9ea016
Aderemi <omoremilekun@gmail.com>
2023-04-04 07:05:07 -0700

    freeing lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - freeing lists
+ * @head: pointer to the pointer of head to be freed
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = ((*head)->next);
+		free(*head);
+		(*head) = temp;
+	}
+	*head = NULL;
+}
