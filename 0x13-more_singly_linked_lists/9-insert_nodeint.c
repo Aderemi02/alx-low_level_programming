414876ac85edeafa3741db15dc0e521995355fa1
Aderemi <omoremilekun@gmail.com>
2023-04-04 16:57:10 -0700

    adding all data in a list in an index

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - insert a node at an index
+ * @head: dounle pointer to the head
+ * @idx: the index
+ * @n: data to be inserted
+ * Return: addtress to new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp, *new;
+	unsigned int m = 1;
+
+	temp = (*head);
+
+	if (!temp)
+	{
+		return (0);
+	}
+
+	new = malloc(sizeof(listint_t));
+
+	while (temp && m < idx)
+	{
+		temp = temp->next;
+		++m;
+	}
+
+	if (!new)
+	{
+		return (0);
+	}
+
+	new->next = temp->next;
+	new->n = n;
+	temp->next = new;
+	return (new);
+}
