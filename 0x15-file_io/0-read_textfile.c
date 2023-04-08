849ae43aac730d6169597ffbdb499ffacfdc8cb7
Aderemi <omoremilekun@gmail.com>
2023-04-08 04:40:23 -0700

    reading textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * read_textfile - reads textfile to standard output
+ * @letters: number of letters to read
+ * @filename: pointer to the file to be read from
+ * Return: number of letters read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *ptr;
+	ssize_t rd, wrt, ch;
+
+	ch = open(filename, O_RDONLY);
+	ptr = malloc(sizeof(char) * letters);
+
+	if (!ch || !ptr)
+		return (0);
+
+	rd = read(ch, ptr, letters);
+	wrt = write(STDOUT_FILENO, ptr, rd);
+	free(ptr);
+	close(ch);
+	return (wrt);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+this is the readme that talks about file handling
