a37d8960483ff46b5987a3285200298fc5410ec2
Aderemi <omoremilekun@gmail.com>
2023-04-08 10:30:06 -0700

    creating new file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * create_file - creates a new file and writes text_content to it
+ * @filename: the file to create
+ * @text_content: the content to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int wrt, opn, stlen = 0;
+
+	if (filename == NULL || text_content == NULL)
+		return (-1);
+
+	while (text_content[stlen] != '\0')
+	{
+		stlen++;
+	}
+
+	opn = open(filename, O_CREAT | O_RDWR);
+	wrt = write(opn, text_content, stlen);
+
+	if (wrt == -1 || opn == -1)
+		return (-1);
+
+	close(opn);
+	return (1);
+}
