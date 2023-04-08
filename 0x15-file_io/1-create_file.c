4fab6c20bc80a209dd616780af0d0fadbedbfde9
Aderemi <omoremilekun@gmail.com>
2023-04-08 14:15:20 -0700

    creating file and writing to it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,6 +5,7 @@
  * @text_content: the content to write to the file
  * Return: 1 on success, -1 on failure
  */
+
 int create_file(const char *filename, char *text_content)
 {
 	int wrt, opn, stlen = 0;
@@ -17,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		stlen++;
 	}
 
-	opn = open(filename, O_CREAT | O_RDWR | O_TRUNC. 0600);
+	opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wrt = write(opn, text_content, stlen);
 
 	if (wrt == -1 || opn == -1)
