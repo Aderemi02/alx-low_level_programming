e5bda2fa056af710f7b7bda34ef11e87f2cde0d5
Aderemi <omoremilekun@gmail.com>
2023-04-08 10:31:03 -0700

    creating new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		stlen++;
 	}
 
-	opn = open(filename, O_CREAT | O_RDWR);
+	opn = open(filename, O_CREAT | O_RDWR | O_TRUNC. 0600);
 	wrt = write(opn, text_content, stlen);
 
 	if (wrt == -1 || opn == -1)
