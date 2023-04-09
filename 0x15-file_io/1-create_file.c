c3757605895910d979199431ca471390523e8f30
Aderemi <omoremilekun@gmail.com>
2023-04-09 02:23:46 -0700

    creatinng file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,10 +12,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-
-	while (text_content[stlen] != '\0')
+	
+	if (text_content != NULL)
 	{
-		stlen++;
+		while (text_content[stlen])
+		{
+			stlen++;
+		}
 	}
 
 	opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
