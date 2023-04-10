7c7be6ba36a6ee32f318baa67dd5247f60e95fd8
Aderemi <omoremilekun@gmail.com>
2023-04-10 06:43:34 -0700

    converting to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_binary - converting to binary using shifts
+ * @n: number to be converted
+ */
+void print_binary(unsigned long int n)
+{
+	int p, T = 0;
+	unsigned int m;
+
+	for (p = 50; p >= 0; p--)
+	{
+		m = n >> p;
+		if (m & 1)
+		{
+			_putchar('1');
+			T++;
+		}
+		else if (T)
+		{
+			_putchar('0');
+		}
+	}
+	if (!T)
+	{
+		_putchar('0');
+	}
+
+}
