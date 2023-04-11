873df257d6850233bcc74e74dd9d1a23217f6f07
Aderemi <omoremilekun@gmail.com>
2023-04-10 18:38:05 -0700

    copying file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -36,14 +36,15 @@ int main(int ac, char *av[])
 		exit(97);
 	}
 	buffer = malloc(sizeof(char) * 1024);
-	file_from = open(av[1], O_RDONLY);
-	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
 
 	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %d\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
+	file_from = open(av[1], O_RDONLY);
+	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
+
 	while (rd == 1024)
 	{
 		rd = read(file_from, buffer, 1024);
