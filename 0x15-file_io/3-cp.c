c7aa35d979f6bb284b8d434ee4be6805346e99f9
Aderemi <omoremilekun@gmail.com>
2023-04-10 18:21:07 -0700

    copying files1

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,7 +10,6 @@ int clos_fil(int fildes)
 	int dess;
 
 	dess = close(fildes);
-
 	if (dess == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fildes);
@@ -28,7 +27,7 @@ int clos_fil(int fildes)
 int main(int ac, char *av[])
 {
 	char *buffer;
-	int opn, opn2, wrt;
+	int file_to, file_from, wrt;
 	ssize_t rd = 1024;
 
 	if (ac != 3)
@@ -39,31 +38,31 @@ int main(int ac, char *av[])
 	buffer = malloc(sizeof(char) * 1024);
 	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-	opn = open(av[1], O_RDONLY);
-	opn2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
+	file_from = open(av[1], O_RDONLY);
+	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
 	while (rd == 1024)
 	{
-		rd = read(opn, buffer, 1024);
-		if (opn == -1 || rd == -1)
+		rd = read(file_from, buffer, 1024);
+		if (file_from == -1 || rd == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			free(buffer);
 			exit(98);
 		}
-		wrt = write(opn2, buffer, rd);
-		if (opn2 == -1 || wrt == -1)
+		wrt = write(file_to, buffer, rd);
+		if (file_to == -1 || wrt == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Cant write to file %s\n", av[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			free(buffer);
 			exit(99);
 		}
 		opn2 = open(av[2], O_WRONLY, 0664);
 	}
 	free(buffer);
-	clos_fil(opn);
-	clos_fil(opn2);
+	clos_fil(file_from);
+	clos_fil(file_to);
 	return (0);
 }
