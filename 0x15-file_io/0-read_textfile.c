2324e5a786aa1144252abd89d84d0b3f2325c102
Aderemi <omoremilekun@gmail.com>
2023-05-08 13:33:51 -0700

    reading and printing letters

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text from and print it
+ * @filename: the file to read from
+ * @letters: number of letters to read and print
+ * Return: number of printed letters
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *pt;
+	ssize_t wrt, opn, rd;
+
+	opn = open(filename, O_RDONLY);
+	pt = malloc(sizeof(pt) * letters);
+
+	if (filename == NULL || pt == NULL)
+		return (0);
+
+	rd = read(opn, pt, letters);
+	wrt = write(STDOUT_FILENO, pt, rd);
+	free(pt);
+	close(opn);
+	return (wrt);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+This is for creating, opening, appending and closing file
