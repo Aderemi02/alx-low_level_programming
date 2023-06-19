50e73241814ecb43c3c1f21412dd702a996e3b1a
Aderemi <omoremilekun@gmail.com>
2023-06-19 12:23:32 -0700

    creating dynamic library for python

---
+++ 0x18-dynamic_libraries/maths.c
@@ -0,0 +1,57 @@
+#include "lists.h"
+/**
+ * add - adding two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - minus two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - reminder of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
